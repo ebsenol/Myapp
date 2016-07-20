579ead7a24a0fee12f1191427e3f4f07be9f9b48
ebsenol <ebsenol@gmail.com>
2016-07-20 17:41:30 +0300

    message

---
+++ app/src/main/java/com/example/senolb/project/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.senolb.project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToPage1(View view){
+        Intent intent = new Intent(this, Page1.class);
+        //String message = editText.getText().toString();
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/senolb/project/Page1.java
@@ -0,0 +1,60 @@
+package com.example.senolb.project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Page1 extends AppCompatActivity {
+    private TextView myText = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page1);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+  /*      EditText text = (EditText)findViewById(R.id.first_number);
+        String numStr=text.getText().toString();
+        int num=0;
+        if (numStr.length()>0) {
+            try {
+                num = Integer.parseInt(numStr);
+            } catch (Exception e) {
+                Log.e("logtag", "Exception: " + e.toString());
+            }
+        }
+        // num *= num;
+        TextView text2= (TextView)findViewById(R.id.first_text);
+        //LinearLayout lView = new LinearLayout(this);
+
+        //setContentView(lView);
+        text2.setText("Square of the number is " + num);
+        */
+    }
+    public void computeSquare(View view){
+        EditText text = (EditText)findViewById(R.id.first_number);
+        String numStr=text.getText().toString();
+      //  Intent intent = new Intent();
+        //String message = editText.getText().toString();
+        int num = 0;
+        if (numStr.length()>0) {
+            try {
+                num = Integer.parseInt(numStr);
+            } catch (Exception e) {
+                Log.e("logtag", "Exception: " + e.toString());
+            }
+        }
+        TextView text2= (TextView)findViewById(R.id.first_text);
+        num = num*num;
+        text2.setText("Square of the number is " + num);
+       // startActivity(intent);
+    }
+}
