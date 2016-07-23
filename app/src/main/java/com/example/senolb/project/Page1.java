4fe0e271d25ebae8b5281cb4d8a9b75902e6b0b0
ebsenol <ebsenol@gmail.com>
2016-07-23 23:29:22 +0300

    message

--- app/src/main/java/com/example/senolb/project/Page1.java
+++ app/src/main/java/com/example/senolb/project/Page1.java
@@ -13,6 +13,8 @@ import android.widget.Toast;
 import retrofit2.Response;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
@@ -26,51 +28,48 @@ public class Page1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page1);
-      //  ImageView imageView = (ImageView) findViewById(R.id.imageView1);
-     //   Glide.with(this).load("https://media.giphy.com/media/LyJ6KPlrFdKnK/giphy.gif").into(imageView);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+       //Glide.with(getApplicationContext()).load("https://media.giphy.com/media/LyJ6KPlrFdKnK/giphy.gif").into(imageView);
+        //Glide.with(this).load("https://media4.giphy.com/media/Y5GVgQZCluUWQ/giphy.gif").into(imageView);
+       // GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(imageView);
+        //Glide.with(this).load("https://media4.giphy.com/media/Y5GVgQZCluUWQ/giphy.gif").into(imageViewTarget);
     }
     public void request(View view) throws IOException {
 
         TextView text2= (TextView)findViewById(R.id.first_text);
 
-        ApiInterface service = ApiInterface.retrofit.create(ApiInterface.class);
-        Call<Downsized> myDownsized = service.getDownsized("dc6zaTOxFJmzC","json","funny","1");
+       // ApiInterface service = ApiInterface.retrofit.create(ApiInterface.class);
+      //  Call<Downsized> myDownsized = service.getDownsized("dc6zaTOxFJmzC","json","funny","1");
         //Downsized a= myDownsized.execute().body();
         //text2.setText(a.getUrl());
-     myDownsized.enqueue(new Callback<Downsized>() {
-         @Override
-         public void onResponse(Call<Downsized> call, Response<Downsized> response) {
-             if (response.isSuccessful()) {
-                 //Downsized DownsizedResponse =
-                 //String data = DownsizedResponse.getUrl();
-                 //String id = DownsizedResponse.getId();
-                 TextView text2 = (TextView) findViewById(R.id.first_text);
-                 //testDownsized = response.body();
-                 //  testDownsized.setUrl(response.body().getUrl());
-                 //   text2.setText("response");
+        EditText name = (EditText) findViewById(R.id.first_number);
+        String keyword = name.getText().toString();
+        ApiInterface service = ApiInterface.retrofit.create(ApiInterface.class);
+        Call<JsonResponse> myDownsized = service.getDownsized("dc6zaTOxFJmzC", "json", keyword, "1");
 
-                 //        text2.setText("response.isSuccess");
-                 //     if (response.body()!=null)
-                Downsized dw = response.body();
-                 text2.setText(dw.getUrl());
-                 // text2.setText(response.body().getUrl());
-                 //if(response.body().getUrl()==null)
-                 // text2.setText("getUrl returns null");
-                 //text2.setText(testDownsized.getUrl()+"aa");
-                 //  adapter = new DataAdapter(data);
-                 //  recyclerView.setAdapter(adapter);
-               /*
-               TextView text2= (TextView)findViewById(R.id.first_text);
-               text2.setText(data);
-               data = DownsizedResponse.getSlug();
-               text2.setText(data);*/
-                 //  data = DownsizedResponse.getSlug();*/
-             } else {
-                 //unsuccessful response
-             }
-         }
+        myDownsized.enqueue(new Callback<JsonResponse>() {
+            @Override
+            public void onResponse(Call<JsonResponse> call, Response<JsonResponse> response) {
+                if (response.isSuccessful()) {
+                    for (Data data : response.body().getDataList()) {
+                        //System.out.println(data.getImages().getDownsized().getUrl());
+                        TextView text2 = (TextView) findViewById(R.id.first_text);
+                        //Downsized dw = response.body();
+                        text2.setText(data.getImages().getDownsized().getUrl());
+                        ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+                        GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(imageView);
+                        Glide.with(getApplicationContext()).load(data.getImages().getDownsized().getUrl()).into(imageViewTarget);
+                       // Glide.load(data.getImages().getDownsized().getUrl()).into(imageView);
+                       // Glide.with(getApplicationContext()).load(data.getImages().getDownsized().getUrl()).into(imageView);
+
+                    }
+
+                } else {
+                    //unsuccessful response
+                }
+            }
          @Override
-         public void onFailure(Call<Downsized> call, Throwable t) {
+         public void onFailure(Call<JsonResponse> call, Throwable t) {
              Log.d("Error", t.getMessage());
              TextView text2 = (TextView) findViewById(R.id.first_text);
              text2.setText(t.getMessage());
